f75fc9e515fe15b50e8a0ed937cd3d74c20ca82c
kaushikkannan17 <kaushikkannan6@gmail.com>
2017-04-03 15:06:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content = {
+    title: "Article One| Kaushik Kannan",
+    heading: "Article One",
+    date: "Feb 14,2017",
+    content:' <p>
+              This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.
+          </p> 
+          <p>
+              This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.
+          </p> 
+          <p>
+              This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.
+          </p> '
+    
+};
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
