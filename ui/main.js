a836184f2a52dacf4a0da811399d39f22ebecb55
kaushikkannan17 <kaushikkannan6@gmail.com>
2017-04-04 15:05:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
-//move the image
+// Move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginleft = '100px';
