57b0f336e5f7e47626ee0118aae3f3f9c62223bb
kaushikkannan17 <kaushikkannan6@gmail.com>
2017-04-12 16:44:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'kaushikkannan17',
+  database: 'kaushikkannan17',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -41,6 +50,7 @@ var articles = {
     </p>` 
 }
 };
+
 function createTemplate (data) {
 var title = data.title;
 var date = data.date;
@@ -79,6 +89,13 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+   //make a select request
+   //return a response with the results
+
 });
 
 var counter = 0;
